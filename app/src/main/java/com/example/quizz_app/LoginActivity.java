2fef82fed338d8f2d9e8492b30910501c3bde6e0
theophile <theophilevonck@gmail.com>
2020-02-13 11:58:00 +0100

    mainV1.1

--- app/src/main/java/com/example/quizz_app/LoginActivity.java
+++ app/src/main/java/com/example/quizz_app/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.quizz_app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,7 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private CheckBox stay_conected;
     private EditText loginEdit;
     private EditText passwordEdit;
 
@@ -19,15 +22,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stay_conected=(CheckBox)findViewById(R.id.stay_conected);
 
         loginEdit = (EditText) findViewById(R.id.loginEditText);
         passwordEdit = (EditText) findViewById(R.id.passwordEditText);
 
         findViewById(R.id.loginButton).setOnClickListener(this);
+
+        myPreferences = getPreferences(MODE_PRIVATE);
+        editor=myPreferences.edit();
+
+        checkPreferences();
     }
 
     @Override
     public void onClick(View v) {
+
 
         if (TextUtils.isEmpty(loginEdit.getText()) && !(TextUtils.isEmpty(passwordEdit.getText())))
         {
@@ -49,8 +59,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     .show();
             return;
         }
-        PreferenceUtils.setLogin(login);
+        preferencesUtils.setLogin(login);
         startActivity(getHomeIntent(login));
+    }
+
+    private void checkPreferences(){
+        EditText usernameEditText = findViewById(R.id.loginEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        String checkbox=myPreferences.getString(getString(R.string.checkboxPref),"remember me");
+        String username=myPreferences.getString(getString(R.string.usernamePref),"");
+        String password=myPreferences.getString(getString(R.string.passwordPref),"");
+
+        usernameEditText.setText(username);
+        passwordEditText.setText(password);
+        if(checkbox.equals("True")){
+            stay_conected.setText("True");
+        }
+        else{
+            stay_conected.setText("remember me");
+        }
+    }
+
+    private Intent getHomeIntent(String userName){
+        final Intent homeIntent = new Intent(this, MainActivity.class);
+        final Bundle extras = new Bundle();
+        extras.putString(SyncStateContract.Constants.Login.EXTRA_LOGIN, userName);
+        homeIntent.putExtras(extras);
+        return homeIntent;
     }
 
 }
---
+++ app/src/main/java/com/example/quizz_app/utils/preferencesUtils.java
@@ -0,0 +1,10 @@
+package com.example.quizz_app.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class preferencesUtils {
+
+   
+
+}
